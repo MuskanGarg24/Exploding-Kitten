e4075448b2dcfaca9f1e4ce120780e5117bbcf8e
muskan garg <muskangarg02270@gmail.com>
2024-02-24 22:44:09 +0530

    fix allowed origins url

--- backend/main.go
+++ backend/main.go
@@ -38,7 +38,7 @@ func main() {
 
 	// Setup CORS middleware
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"https://exploding-kitten-seven.vercel.app/"},
+		AllowedOrigins:   []string{"https://exploding-kitten-seven.vercel.app"},
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		Debug:            true,
